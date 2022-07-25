8fc5859bbf67c9608a953896ab346e2996706c49
yadlapalli9999 <yadlapalli9999@gmail.com>
2022-07-25 15:21:23 +0530

    create layout page and routing also

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './layout/components/home/Home';
+import Navbar from './layout/components/navbar/Navbar';
+import BenchHome from './modules/bench/BenchHome';
+import POCHome from './modules/poc/POCHome';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+       
+       <BrowserRouter>
+       <Navbar/>
+         <Routes>
+            <Route path='/' element={<Home/>}/>
+            <Route path='/poc' element={<POCHome/>}/>
+            <Route path='/bench' element={<BenchHome/>}/>
+         </Routes>
+       </BrowserRouter>
+    </React.Fragment>
   );
 }
 
---
+++ frontend/src/modules/poc/POCHome.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+let POCHome = ()=>{
+    return(
+        <React.Fragment>
+            <h2>POC Home</h2>
+        </React.Fragment>
+    )
+}
+export default POCHome;
